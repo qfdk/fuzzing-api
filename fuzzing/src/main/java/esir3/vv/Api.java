5c4cbf0edf696dfdfbdd729d48feb4c617bdb352
ymaiga <maiga93@yahoo.fr>
2016-11-16 00:39:38 +0100

    Get all path of a swagger specification

--- fuzzing/src/main/java/esir3/vv/Api.java
+++ fuzzing/src/main/java/esir3/vv/Api.java
@@ -1,11 +1,16 @@
 package esir3.vv;
 
 
+import java.util.Map;
+
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+
+import io.swagger.models.Swagger;
+import io.swagger.parser.SwaggerParser;
 
 /**
  * Created by qfdk on 19/10/2016.
@@ -13,22 +18,31 @@ import javax.ws.rs.core.Response;
 @Path("v1")
 public class Api {
 
-    public Api() {
-        System.out.println("[+] Fuzzing api init...");
-    }
-    /**
-     * http://localhost:8080/api/v1/getUrl
-     *
-     * @return
-     * @throws Exception
-     */
-    @GET
-    @Path("getStatus")
-    @Produces(MediaType.APPLICATION_JSON)
-    public Response getUrl() throws Exception {
-        return Response.status(200).entity("{status:ok,msg:api works!}").build();
-    }
+	public Api() {
+		System.out.println("[+] Fuzzing api init...");
 
 
+	}
+	/**
+	 * http://localhost:8080/api/v1/getUrl
+	 *
+	 * @return
+	 * @throws Exception
+	 */
+	@GET
+	@Path("getStatus")
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response getUrl() throws Exception {
 
+		Swagger swagger = new SwaggerParser().read("./src/main/resources/swagger.json");
+
+		Map<String, io.swagger.models.Path> paths = swagger.getPaths();
+
+		System.out.println("------- List of all paths ----------");
+		for(Map.Entry<String, io.swagger.models.Path> entry : paths.entrySet()) {
+			System.out.println(entry.getKey());
+		}
+
+		return Response.status(200).entity("{status:ok,msg:api works!}").build();
+	}
 }
