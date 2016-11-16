dcf5c74c6e7122fbb418343bf175e5e69968ed22
qfdk <qfdk@vip.qq.com>
2016-11-16 17:52:37 +0100

    add analyer

--- fuzzing/src/main/java/esir3/vv/Api.java
+++ fuzzing/src/main/java/esir3/vv/Api.java
@@ -1,6 +1,7 @@
 package esir3.vv;
 
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
@@ -12,6 +13,7 @@ import javax.ws.rs.core.Response;
 
 import io.swagger.models.Swagger;
 import io.swagger.parser.SwaggerParser;
+import org.json.JSONObject;
 
 /**
  * Created by qfdk on 19/10/2016.
@@ -58,17 +60,27 @@ public class Api {
     @Path("analyse")
     @Produces(MediaType.APPLICATION_JSON)
     public Response analyse() throws Exception {
+        List<String> urls=new ArrayList<>();
+        urls.add("http://qfdk.me");
+        urls.add("http://google.com");
+        data = new FuzzingData("localhost","html",urls);
 
-//        List<String> paths=data.getPaths();
-//        String hostname=data.getHostname();
-//        String contentType= data.getContentType();
-//
-//        for(String path:paths)
-//        {
-//
-//        }
-        System.out.println(Tools.sendGet("http://qfdk.me"));
+        List<String> paths=data.getPaths();
+        List<String> pathsValided= new ArrayList<>();
 
-        return Response.status(200).entity("{status:ok,msg:sasa}").build();
+        String hostname=data.getHostname();
+        String contentType= data.getContentType();
+
+        JSONObject jsonObject = new JSONObject();
+
+        jsonObject.put("hostname",hostname);
+        jsonObject.put("contentType",contentType);
+
+        for(String path:paths)
+        {
+            pathsValided.add(Tools.sendGet(path).split("#")[0]+"#"+path);
+        }
+        jsonObject.put("paths",pathsValided);
+        return Response.status(200).entity(jsonObject.toString()).build();
     }
 }
