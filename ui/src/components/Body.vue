43f368dfd079818a6a0bfd7c0264e6f109dc5297
HAOZHI Li <lhaozhi@jouve.fr>
2016-12-30 16:27:55 +0100

    [update] add alert info

--- ui/src/components/Body.vue
+++ ui/src/components/Body.vue
@@ -19,90 +19,80 @@
     </div>
     <br>
     <div class="jumbotron" style="background-color: #fff9f0;">
-      <!-- <table class="table">
-      <thead>
-      <tr>
-      <th>#No.</th>
-      <th>Target url</th>
-      <th>Reponse code</th>
-      <th>Result list</th>
-      <th>Operation Type</th>
-    </tr>
-  </thead>
+      <p v-show="init_info" class="alert alert-info text-center">Please entre a <strong>swagger.json</strong> path then click go!</p>
+      <accordion :one-at-atime="checked" :type="selected">
+        <panel v-for="(res,index) in results" v-bind:type="[res.valided ? 'success':'danger']" >
+          <h5 slot="header">
+            <strong># {{ index+1 }}</strong> {{res.operationType}} {{res.link}}
+          </h5>
+          <table class="table table-condensed">
+            <thead>
+              <tr>
+                <th>Type</th> <th>reponse code</th><th>Reponse code</th><th>Parameters</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>{{res.operationType}}</td><td>{{res.codes}}</td> <td>{{res.reponseCode}}</td><td>{{res.parameters}}</td>
+              </tr>
+            </tbody>
+          </table>
+        </panel>
+      </accordion>
+    </div>
+    <spinner ref="spinner" :size="size" :fixed="fixed" text="I'm doing my work now... " style="display: none;"></spinner>
+    <hr/>
+    <alert v-model="showRight" placement="top-right" duration="5000" type="info" width="400px" dismissable>
+      <span class="icon-ok-circled alert-icon-float-left"></span>
+      <strong>Well Done!</strong>
+      <p>You successfully read this important alert message :)</p>
+    </alert>
+  </template>
 
-  <tbody>
-  <tr v-for="(res,index) in results" v-bind:class="[res.valided ? 'success':'danger']">
-  <td>{{index + 1}}</td>
-  <td>{{res.link}}</td>
-  <td>{{res.reponseCode}}</td>
-  <td>{{res.codes}}</td>
-  <td>{{res.operationType}}</td>
-</tr>
-</tbody>
-
-</table> -->
-
-<accordion :one-at-atime="checked" :type="selected">
-  <panel v-for="(res,index) in results" v-bind:type="[res.valided ? 'success':'danger']" >
-    <h5 slot="header">
-    <strong># {{ index+1 }}</strong> {{res.operationType}} {{res.link}}
-  </h5>
-    <table class="table table-condensed">
-      <thead>
-        <tr>
-          <th>Type</th> <th>reponse code</th><th>Reponse code</th><th>Parameters</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr>
-          <td>{{res.operationType}}</td><td>{{res.codes}}</td> <td>{{res.reponseCode}}</td><td>{{res.parameters}}</td>
-        </tr>
-      </tbody>
-    </table>
-  </panel>
-</accordion>
-</div>
-<spinner ref="spinner" :size="size" :fixed="fixed" text="I'm doing my work now... " style="display: none;"></spinner>
-<hr/>
-</template>
-
-<script>
+  <script>
 
 
-import spinner from 'vue-strap/src/spinner'
-import accordion from 'vue-strap/src/accordion'
-import panel from 'vue-strap/src/Panel'
+  import alert from 'vue-strap/src/Alert'
+  import spinner from 'vue-strap/src/Spinner'
+  import accordion from 'vue-strap/src/Accordion'
+  import panel from 'vue-strap/src/Panel'
 
-export default {
-  name:'middle',
-  components:{
-    spinner,
-    accordion,
-    panel
-  },
-  data(){
-    return {
-      url:"http://petstore.swagger.io/v2/swagger.json",
-      results:"",
-      fixed:true,
-      size:'lg',
-      selected: 'info'
-    }
-  },
-  methods:{
-    go:function()
-    {
-      this.$refs.spinner.show()
-      var tmp='http://'+window.location['hostname']+':8080/api/v1/getPath?url=';
-      this.$http.get(tmp+this.url).then((result) => {
-        this.results=result.body.urls;
-        this.$refs.spinner.hide()
-      }, (error) => {
-      })
+  export default {
+    name:'middle',
+    components:{
+      spinner,
+      accordion,
+      panel,
+      alert
+    },
+    data(){
+      return {
+        url:"http://petstore.swagger.io/v2/swagger.json",
+        results:"",
+        fixed:true,
+        size:'lg',
+        selected: 'info',
+        checked:true,
+        init_info:true,
+        showRight:false
+      }
+    },
+    methods:{
+      go:function()
+      {
+        this.$refs.spinner.show()
+        this.init_info=false;
+        var tmp='http://'+window.location['hostname']+':8080/api/v1/getPath?url=';
+        this.$http.get(tmp+this.url).then((result) => {
+          this.results=result.body.urls;
+          this.$refs.spinner.hide()
+          this.showRight=true;
+        }, (error) => {
+        })
+      }
     }
   }
-}
-</script>
+  </script>
 
-<style lang="css">
-</style>
+  <style lang="css">
+  </style>
